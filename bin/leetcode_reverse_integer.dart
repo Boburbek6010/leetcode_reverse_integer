cc31e499dfe1b389b5f5dafbbd08baa31e09dcb9
Boburbek6010 <boburbekking@gmail.com>
2022-04-06 10:37:04 +0500

    Unedited solved tasks from leetcode

---
+++ bin/leetcode_reverse_integer.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'dart:math';
+
+void main() {
+  /**1-masala**/
+  // print('Raqamni kiriting => ');
+  // int n = int.parse(stdin.readLineSync()!);
+  // num len = n.toString().length;
+  // print(len);
+  // int last = 0;
+  // num sum = 0;
+  // if(!n.toString().contains('-')){
+  //   while(n > 0){
+  //     len = pow(10, len-1);
+  //     last = n % 10;
+  //     sum += last * len;
+  //     n = n ~/ 10;
+  //     len = n.toString().length;
+  //   }
+  //   print(sum);
+  // }else{
+  //   String s = n.toString().replaceRange(0, 1, '');
+  //   n = int.parse(s);
+  //   len = n.toString().length;
+  //   while(n > 0){
+  //     len = pow(10, len-1);
+  //     last = n % 10;
+  //     sum += last * len;
+  //     n = n ~/ 10;
+  //     len = n.toString().length;
+  //   }
+  //   print(sum.toString().replaceRange(0, 0, '-'));
+  // }
+
+  /**2-masala**/
+  // print('N => ');
+  // int n = int.parse(stdin.readLineSync()!);
+  // List list = [];
+  //   int odd = 0;
+  //   int t = 0;
+  //   for(int i = 1; i <= n; i++){
+  //     odd = 0;
+  //     for (int j = 1; j <= i; j++) {
+  //       if (i % j == 0) {
+  //         odd++;
+  //       }
+  //     }
+  //     if(odd == 2){
+  //       t++;
+  //       list.add(i);
+  //     }
+  //   }
+  //   print('\n$n gacha bo\'lgan tub sonlar $list');
+  //   print('Va ularning soni: $t');
+}
---
+++ test/leetcode_reverse_integer_test.dart
@@ -0,0 +1,8 @@
+import 'package:leetcode_reverse_integer/leetcode_reverse_integer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
